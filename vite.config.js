eb926f59089ea8e3959f7203266c5284094a246e
Netero03 <jatinletsgo@gmail.com>
2024-07-20 23:38:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,9 @@ import envCompatible from 'vite-plugin-env-compatible';
 export default defineConfig({
   envPrefix: "REACT_APP_",
   plugins: [react(),
-    envCompatible()
+  envCompatible()
   ],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-    }
+
+  }
 })
