b7653dada29c5cad213bf9d15f306255f2df1295
your <jaiswalanshikaajay.7@gmail.com>
2024-07-22 21:50:08 +0530

    agents

--- src/components/layout/Contact.jsx
+++ src/components/layout/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
       <Container>
         {/* contact */}
         <div className='flex justify-between '>
-          <h6 className='text-white text-xs xl:text-sm self-center cursor-pointer'>support@yoursupportagent.com</h6>
-          <button className='bg-white hover:bg-[#253359] border text-[#253359] hover:text-[#fff] lg:font-semibold text-xs xl:text-sm py-1 px-3 mr-2 rounded'>CALL NOW</button>
+          <h6 className='text-white text-xs 2xl:text-sm self-center cursor-pointer'>support@yoursupportagent.com</h6>
+          <button className='bg-white hover:bg-[#253359] border text-[#253359] hover:text-[#fff] 2xl:font-semibold text-xs xl:text-sm py-1 px-3 mr-2 rounded'>CALL NOW</button>
         </div>
       </Container>
       </div>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -18,18 +18,18 @@ const Navbar = () => {
         </div>
         <div className='flex gap-12 '>
           <nav className='flex gap-5 items-center'>
-            <a href='/#industries' className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Industries</a>
-            <a href='/#individuals' className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Individual Roles</a>
-            <a href='/#marketing' className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Marketing Service</a>
-            <a href='/#custom' className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Custom Service</a>
-            <a className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>About Us</a>
-            <a href='/#resources' className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Resources</a>
-            <a className='text-[#253359] font-semibold text-xs xl:text-sm  2xl:text-base'>Pricing</a>
+            <a href='/#industries' className='text-[#253359] font-semibold text-xs   2xl:text-base'>Industries</a>
+            <a href='/#individuals' className='text-[#253359] font-semibold text-xs   2xl:text-base'>Individual Roles</a>
+            <a href='/#marketing' className='text-[#253359] font-semibold text-xs   2xl:text-base'>Marketing Service</a>
+            <a href='/#custom' className='text-[#253359] font-semibold text-xs   2xl:text-base'>Custom Service</a>
+            <a className='text-[#253359] font-semibold text-xs   2xl:text-base'>About Us</a>
+            <a href='/#resources' className='text-[#253359] font-semibold text-xs   2xl:text-base'>Resources</a>
+            <a className='text-[#253359] font-semibold text-xs   2xl:text-base'>Pricing</a>
 
           </nav>
           <div>
             <Link to='/marketplace'>
-            <button className='hover:bg-[#e0efe3] text-white rounded-3xl py-2.5 px-5 text-xs xl:text-sm 2xl:text-base btn btn1'>Book a Call</button>
+            <button className='hover:bg-[#e0efe3] text-white rounded-3xl py-2.5 px-5 text-xs  2xl:text-base btn btn1'>Book a Call</button>
             </Link>
           </div>
         </div>
